9c7cceef88a406e38b8e743f1dde5c107878bfdf
chenyx <799771565@qq.com>
2019-07-30 16:22:48 +0800

    add location respository

---
+++ git-project/src/com/chenyx/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.chenyx.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("ni hao");
+        System.out.println("ni hao");
+        System.out.println("ni hao");
+        System.out.println("ni hao");
+    }
+
+}
